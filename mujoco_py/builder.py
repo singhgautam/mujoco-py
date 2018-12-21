0430351c6f58ec96d3f7768b65c0d52d2bc6be62
Gautam Singh <singh.gautam@rutgers.edu>
2018-12-21 14:51:56 -0500

    mujoco_py fix

--- mujoco_py/builder.py
+++ mujoco_py/builder.py
@@ -67,8 +67,7 @@ The easy solution is to `import mujoco_py` _before_ `import glfw`.
         Builder = MacExtensionBuilder
     elif sys.platform == 'linux':
         _ensure_set_env_var("LD_LIBRARY_PATH", lib_path)
-        if os.getenv('MUJOCO_PY_FORCE_CPU') is None and get_nvidia_lib_dir() is not None:
-            _ensure_set_env_var("LD_LIBRARY_PATH", get_nvidia_lib_dir())
+        if os.getenv('MUJOCO_PY_FORCE_CPU') is None:
             Builder = LinuxGPUExtensionBuilder
         else:
             Builder = LinuxCPUExtensionBuilder
